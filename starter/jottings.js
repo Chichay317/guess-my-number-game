d8c6eedd08f39334b50b3d891cafac42119d931d
Chichay317 <chic11951@gmail.com>
2023-01-25 13:07:11 +0100

    initial commit

---
+++ starter/jottings.js
@@ -0,0 +1,10 @@
+// When using addEventListener, the first argument you pass is the type of the event
+  // After passing the type of event, you pass the reaction you want to get after clicking through a function
+  // The function is called the event handler
+  // We do not call the function
+// When we get something from a user interface e.g an input, the value is usually a string
+// To compare a number with a number, first convert the string to a number
+// We use math.trunc to remove the decimals of a decimal number
+// Math is an object with so many methods. random is just one of the methods
+// Whenever we are manipulating a style, we always need to specify a string
+// The first step of refactoring is to identify duplicate code
